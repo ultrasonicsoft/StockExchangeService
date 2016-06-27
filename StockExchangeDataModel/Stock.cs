4b3495ab776e281f2159fdb324ae11c3f0f84635
ultrasonicsoft <uvbrainsolutions@outlook.com>
2016-06-27 10:15:27 +0530

    CodeMap generated for solution

--- StockExchangeDataModel/Stock.cs
+++ StockExchangeDataModel/Stock.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace StockExchange
+﻿namespace StockExchangeDataModel
 {
     public class Stock
     {
--- StockExchangeManager/StockExchangeDataProvider.cs
+++ StockExchangeManager/StockExchangeDataProvider.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using StockExchange.Service;
+using StockExchangeDataModel;
 using StockExchangeRepository.Model;
 
 namespace StockExchange
